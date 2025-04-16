0fde180031e56d38ac53f132edce2ba59cd0bd1d
BadrGello <138599821+BadrGello@users.noreply.github.com>
2025-04-16 15:40:36 +0200

    Fixed routh was always showing "Stable"

--- react-frontend/src/components/pages/routh.jsx
+++ react-frontend/src/components/pages/routh.jsx
@@ -82,7 +82,7 @@ function RouthForm() {
                     <h2>Number of roots in the right half-plane: {result.RHS_roots_num}</h2>
                     <h2>Number of roots in the left half-plane: {result.LHS_roots_num}</h2>
                     <h2>Number of roots on the imaginary axis: {result.Jw_axis_roots_num}</h2>
-                    <h2 style={{color: "#f6c177", fontFamily: "'IBM Plex Mono', monospace" }}>System Stability: {result.stable?"Stable":"Unstable"}</h2>
+                    <h2 style={{color: "#f6c177", fontFamily: "'IBM Plex Mono', monospace" }}>System Stability: {result.stable}</h2>
 
                 </div>
             )}
